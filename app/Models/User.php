5bd9f72edf6f61c1ebf3157e92522c84963bc612
rjp2525 <me@renophilibert.com>
2015-04-20 14:09:23 -0400

    Implement soft deleting on user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, CanResetPassword, SoftDeletes;
 
     /**
      * The database table used by the model.
@@ -30,5 +31,12 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * Set the soft delete column name
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
 }
