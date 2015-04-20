74bb1f6501c9762dc46c0f355f9caf10150b9ad8
rjp2525 <me@renophilibert.com>
2015-04-20 13:11:07 -0400

    Configurable Twitter package settings via .env

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -3,7 +3,7 @@
 // You can find the keys here : https://apps.twitter.com/
 
 return [
-	'debug'               => false,
+	'debug'               => env('TWITTER_DEBUG', false),
 
 	'API_URL'             => 'api.twitter.com',
 	'UPLOAD_URL'          => 'upload.twitter.com',
@@ -12,10 +12,10 @@ return [
 	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
 	'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
-	'USE_SSL'             => true,
+	'USE_SSL'             => env('TWITTER_USE_SSL', true),
 
-	'CONSUMER_KEY'        => '',
-	'CONSUMER_SECRET'     => '',
-	'ACCESS_TOKEN'        => '',
-	'ACCESS_TOKEN_SECRET' => '',
+	'CONSUMER_KEY'        => env('TWITTER_CONSUMER_KEY', ''),
+	'CONSUMER_SECRET'     => env('TWITTER_CONSUMER_SECRET', ''),
+	'ACCESS_TOKEN'        => env('TWITTER_ACCESS_TOKEN', ''),
+	'ACCESS_TOKEN_SECRET' => env('TWITTER_ACCESS_TOKEN_SECRET', ''),
 ];
