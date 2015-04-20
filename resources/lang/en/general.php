b2eaaf082501688b97200839b2f49ef382fecdef
rjp2525 <me@renophilibert.com>
2015-04-20 14:34:31 -0400

    General language lines

---
+++ resources/lang/en/general.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Genera; Application Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used for general language throughout the
+	| application. This is simply to future-proof everything.
+	|
+	*/
+
+	"site" => [
+		"title" => "Falcon Frag",
+	],
+
+];
