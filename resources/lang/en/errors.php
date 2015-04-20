047ed327881d07e5f4d87a99d4e0d4a6a89295d8
rjp2525 <me@renophilibert.com>
2015-04-20 14:30:54 -0400

    Error language file

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Error Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used for errors all throughout the
+	| application. This is simply to future-proof everything.
+	|
+	*/
+
+	"account" => [
+		"exists" => "The credentials you entered were incorrect. Please verify them and try again.",
+		"deleted" => "The account you are trying to access has been deleted or terminated. Please contact support if you believe this is an error.",
+		"failed" => "The credentials you entered were incorrect. Please verify them and try again.",
+	],
+
+];
