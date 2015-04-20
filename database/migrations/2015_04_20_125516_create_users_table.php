e49655b41ab5bf9d6544274032e219eafe4bc4a1
rjp2525 <me@renophilibert.com>
2015-04-20 14:07:27 -0400

    Add fields to user table schema

--- database/migrations/2015_04_20_125516_create_users_table.php
+++ database/migrations/2015_04_20_125516_create_users_table.php
@@ -15,6 +15,15 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('company')->nullable();
+			$table->string('username')->unique();
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->string('activation_code')->nullable();
+			$table->boolean('active')->default(false);
+			$table->softDeletes();
 			$table->timestamps();
 		});
 	}
