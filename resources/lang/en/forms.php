d89aab705782a5141445be65e63a8abc46b51d27
rjp2525 <me@renophilibert.com>
2015-04-20 15:53:50 -0400

    Form language

---
+++ resources/lang/en/forms.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Form Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines provide translations for forms.
+	|
+	*/
+
+	"register" => [
+		"labels" => [
+			"first_name" => "First Name",
+			"last_name" => "Last Name",
+			"company" => "Company",
+			"username" => "Username",
+			"email" => "Email",
+			"password" => "Password",
+			"password_repeat" => "Confirm Password",
+		],
+		"placeholders" => [
+			"first_name" => "First Name",
+			"last_name" => "Last Name",
+			"company" => "Company",
+			"username" => "Username",
+			"email" => "Email",
+			"password" => "Password",
+			"password_repeat" => "Confirm Password",
+		],
+	],
+
+];
