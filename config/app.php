2add64c799bc5c6bb40bced156087a394a7609c0
rjp2525 <me@renophilibert.com>
2015-05-07 15:34:56 -0400

    Add HTML Purifier package

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
 	| stack traces will be shown on every error that occurs within your
 	| application. If disabled, a simple generic error page is shown.
 	|
-	*/
+	 */
 
 	'debug' => env('APP_DEBUG'),
 
@@ -24,7 +24,7 @@ return [
 	| the Artisan command line tool. You should set this to the root of
 	| your application so that it is used when running Artisan tasks.
 	|
-	*/
+	 */
 
 	'url' => env('APP_URL', 'http://localhost'),
 
@@ -37,7 +37,7 @@ return [
 	| will be used by the PHP date and date-time functions. We have gone
 	| ahead and set this to a sensible default for you out of the box.
 	|
-	*/
+	 */
 
 	'timezone' => env('APP_TIMEZONE', 'UTC'),
 
@@ -50,7 +50,7 @@ return [
 	| by the translation service provider. You are free to set this value
 	| to any of the locales which will be supported by the application.
 	|
-	*/
+	 */
 
 	'locale' => 'en',
 
@@ -63,7 +63,7 @@ return [
 	| is not available. You may change the value to correspond to any of
 	| the language folders that are provided through your application.
 	|
-	*/
+	 */
 
 	'fallback_locale' => 'en',
 
@@ -76,7 +76,7 @@ return [
 	| to a random, 32 character string, otherwise these encrypted strings
 	| will not be safe. Please do this before deploying an application!
 	|
-	*/
+	 */
 
 	'key' => env('APP_KEY', 'SomeRandomString'),
 
@@ -93,7 +93,7 @@ return [
 	|
 	| Available Settings: "single", "daily", "syslog", "errorlog"
 	|
-	*/
+	 */
 
 	'log' => 'daily',
 
@@ -106,7 +106,7 @@ return [
 	| request to your application. Feel free to add your own services to
 	| this array to grant expanded functionality to your applications.
 	|
-	*/
+	 */
 
 	'providers' => [
 
@@ -150,6 +150,7 @@ return [
 		 */
 		'Collective\Html\HtmlServiceProvider',
 		'Thujohn\Twitter\TwitterServiceProvider',
+		'Mews\Purifier\PurifierServiceProvider',
 
 	],
 
@@ -162,42 +163,42 @@ return [
 	| is started. However, feel free to register as many as you wish as
 	| the aliases are "lazy" loaded so they don't hinder performance.
 	|
-	*/
+	 */
 
 	'aliases' => [
 
-		'App'       => 'Illuminate\Support\Facades\App',
-		'Artisan'   => 'Illuminate\Support\Facades\Artisan',
-		'Auth'      => 'Illuminate\Support\Facades\Auth',
-		'Blade'     => 'Illuminate\Support\Facades\Blade',
-		'Bus'       => 'Illuminate\Support\Facades\Bus',
-		'Cache'     => 'Illuminate\Support\Facades\Cache',
-		'Config'    => 'Illuminate\Support\Facades\Config',
-		'Cookie'    => 'Illuminate\Support\Facades\Cookie',
-		'Crypt'     => 'Illuminate\Support\Facades\Crypt',
-		'DB'        => 'Illuminate\Support\Facades\DB',
-		'Eloquent'  => 'Illuminate\Database\Eloquent\Model',
-		'Event'     => 'Illuminate\Support\Facades\Event',
-		'File'      => 'Illuminate\Support\Facades\File',
-		'Hash'      => 'Illuminate\Support\Facades\Hash',
-		'Input'     => 'Illuminate\Support\Facades\Input',
+		'App' => 'Illuminate\Support\Facades\App',
+		'Artisan' => 'Illuminate\Support\Facades\Artisan',
+		'Auth' => 'Illuminate\Support\Facades\Auth',
+		'Blade' => 'Illuminate\Support\Facades\Blade',
+		'Bus' => 'Illuminate\Support\Facades\Bus',
+		'Cache' => 'Illuminate\Support\Facades\Cache',
+		'Config' => 'Illuminate\Support\Facades\Config',
+		'Cookie' => 'Illuminate\Support\Facades\Cookie',
+		'Crypt' => 'Illuminate\Support\Facades\Crypt',
+		'DB' => 'Illuminate\Support\Facades\DB',
+		'Eloquent' => 'Illuminate\Database\Eloquent\Model',
+		'Event' => 'Illuminate\Support\Facades\Event',
+		'File' => 'Illuminate\Support\Facades\File',
+		'Hash' => 'Illuminate\Support\Facades\Hash',
+		'Input' => 'Illuminate\Support\Facades\Input',
 		'Inspiring' => 'Illuminate\Foundation\Inspiring',
-		'Lang'      => 'Illuminate\Support\Facades\Lang',
-		'Log'       => 'Illuminate\Support\Facades\Log',
-		'Mail'      => 'Illuminate\Support\Facades\Mail',
-		'Password'  => 'Illuminate\Support\Facades\Password',
-		'Queue'     => 'Illuminate\Support\Facades\Queue',
-		'Redirect'  => 'Illuminate\Support\Facades\Redirect',
-		'Redis'     => 'Illuminate\Support\Facades\Redis',
-		'Request'   => 'Illuminate\Support\Facades\Request',
-		'Response'  => 'Illuminate\Support\Facades\Response',
-		'Route'     => 'Illuminate\Support\Facades\Route',
-		'Schema'    => 'Illuminate\Support\Facades\Schema',
-		'Session'   => 'Illuminate\Support\Facades\Session',
-		'Storage'   => 'Illuminate\Support\Facades\Storage',
-		'URL'       => 'Illuminate\Support\Facades\URL',
+		'Lang' => 'Illuminate\Support\Facades\Lang',
+		'Log' => 'Illuminate\Support\Facades\Log',
+		'Mail' => 'Illuminate\Support\Facades\Mail',
+		'Password' => 'Illuminate\Support\Facades\Password',
+		'Queue' => 'Illuminate\Support\Facades\Queue',
+		'Redirect' => 'Illuminate\Support\Facades\Redirect',
+		'Redis' => 'Illuminate\Support\Facades\Redis',
+		'Request' => 'Illuminate\Support\Facades\Request',
+		'Response' => 'Illuminate\Support\Facades\Response',
+		'Route' => 'Illuminate\Support\Facades\Route',
+		'Schema' => 'Illuminate\Support\Facades\Schema',
+		'Session' => 'Illuminate\Support\Facades\Session',
+		'Storage' => 'Illuminate\Support\Facades\Storage',
+		'URL' => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
-		'View'      => 'Illuminate\Support\Facades\View',
+		'View' => 'Illuminate\Support\Facades\View',
 
 		/*
 		 * Package Facades
@@ -205,6 +206,7 @@ return [
 		'Form' => 'Collective\Html\FormFacade',
 		'HTML' => 'Collective\Html\HtmlFacade',
 		'Twitter' => 'Thujohn\Twitter\Facades\Twitter',
+		'Purifier' => 'Mews\Purifier\Facades\Purifier',
 
 	],
 
