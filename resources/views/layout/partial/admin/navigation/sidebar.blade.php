a38ab6b7966f959fab6c7e96cb0febb20b1bdad7
rjp2525 <me@renophilibert.com>
2015-05-23 21:42:14 -0400

    Sidebar is somewhat complete, responsiveness needs work

---
+++ resources/views/layout/partial/admin/navigation/sidebar.blade.php
@@ -0,0 +1,314 @@
+<div class="sidebar-menu toggle-others collapsed fixed">
+    <div class="sidebar-menu-inner">
+        <header class="logo-env">
+            <div class="logo">
+                <a href="#" class="logo-expanded">
+                    <img src="{{ url('img/admin/nav_logo.png') }}" width="80" alt="Falcon Frag">
+                </a>
+                <a href="#" class="logo-collapsed">
+                    <img src="{{ url('img/admin/nav_logo_collapsed.png') }}" width="40" alt="Falcon Frag">
+                </a>
+            </div>
+            <div class="mobile-menu-toggle visible-xs">
+                <a href="#" data-toggle="user-info-menu">
+                    <i class="fa-bell-o"></i>
+                    <span class="badge badge-success">3</span>
+                </a>
+                <a href="#" data-toggle="mobile-menu">
+                    <i class="fa-bars"></i>
+                </a>
+            </div>
+        </header>
+        <ul class="main-menu" id="main-menu">
+            <li>
+                <a href="#">
+                    <i class="fa-line-chart"></i>
+                    <span class="title">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-users"></i>
+                    <span class="title">Clients</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="title">View Clients</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Create Client</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Products &amp; Services</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Cancellation Requests</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Affiliates</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Email Newsletter</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-sitemap"></i>
+                    <span class="title">Orders</span>
+                    <span class="label label-success pull-right">3</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="title">View All Orders</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Pending Orders</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Active Orders</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Fraudulent Orders</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Cancelled Orders</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Create Order</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-credit-card"></i>
+                    <span class="title">Billing</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="title">Transactions</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Invoices</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Billable Items</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Quotes</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Offline Processing</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Gateway Log</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-support"></i>
+                    <span class="title">Support</span>
+                    <span class="label label-danger pull-right">27</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="title">Support Overview</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Announcements</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Knowledgebase</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Support Tickets</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Open Ticket</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Network Issues</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-book"></i>
+                    <span class="title">Reports</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="title">Daily Performance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Income Projections</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Annual Income Report</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">New Customers</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Ticket Feedback Scores</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Batch Invoice PDF Export</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Server Status</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-wrench"></i>
+                    <span class="title">Utilities</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="title">Email Marketer</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Analytics</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Calendar</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">To-Do List</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Domain Tools</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Backups &amp; System</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Activity Logs</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-briefcase"></i>
+                    <span class="title">Company Management</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="title">General Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Automation</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Staff Management</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Payments</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Products &amp; Services</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Email Templates</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Modules</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
