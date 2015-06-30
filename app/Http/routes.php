d832fae4a694e60fa6aad46fa1c67e873da2c09e
rjp2525 <me@renophilibert.com>
2015-06-29 21:34:19 -0400

    Remove Angular + Dingo API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,30 +11,6 @@
 |
  */
 
-/*$api =
-
-ApiRoute::api(['version' => 'v1', 'namespace' => 'Api\V1'], function () {
-// this is gonna be a nightmare
-Route::get('/', 'Auth\AuthController@getIndex');
-});*/
-
-$api = app('api.router');
-
-$api->version('v1', ['prefix' => 'api/v1', 'namespace' => 'Falcon\Http\Controllers\Api\V1'], function ($api) {
-    $api->group(['prefix' => 'auth'], function ($api) {
-        $api->get('/', 'Auth\AuthController@index');
-        $api->post('login', 'Auth\AuthController@login');
-        //$api->get('/', 'Auth\AuthController@index');
-    });
-    //$api->get('/', 'Auth\AuthController@index');
-});
-
-/*Route::api(['version' => 'v1', 'namespace' => 'Api\V1'], function () {
-Route::get('/', 'Auth\AuthController@getIndex');
-});*/
-
 Route::any('{path?}', function () {
     return view('welcome');
 })->where('path', '.+');
-
-//Route::any('{path?}', 'WelcomeController@index')->where("path", ".+");
