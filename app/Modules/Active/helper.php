414d67ffe46dfa4f7b3e1b001d4c14c6d13ec04e
rjp2525 <me@renophilibert.com>
2015-08-04 10:47:57 -0400

    Remove active module and replace with package

--- app/Modules/Active/helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists('active_route')) {
-    function active_route($names, $activeClass = 'active', $inactiveClass = '')
-    {
-        return app('active')->route($names, $activeClass, $inactiveClass);
-    }
-}
