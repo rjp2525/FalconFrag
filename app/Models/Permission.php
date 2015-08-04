11789a931093aaee901401268dcc08668dfffd34
rjp2525 <me@renophilibert.com>
2015-08-04 10:31:42 -0400

    Using bican/roles for permission and role management

---
+++ app/Models/Permission.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Falcon\Models;
+
+use Bican\Roles\Contracts\PermissionHasRelations as PermissionHasRelationsContract;
+use Bican\Roles\Traits\PermissionHasRelations;
+use Bican\Roles\Traits\Slugable;
+use Falcon\Models\Model;
+
+class Permission extends Model implements PermissionHasRelationsContract
+{
+    use Slugable, PermissionHasRelations;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'slug', 'description', 'model'];
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Falcon\Models;
+
+use Bican\Roles\Contracts\RoleHasRelations as RoleHasRelationsContract;
+use Bican\Roles\Traits\RoleHasRelations;
+use Bican\Roles\Traits\Slugable;
+use Falcon\Models\Model;
+
+class Role extends Model implements RoleHasRelationsContract
+{
+    use Slugable, RoleHasRelations;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'slug', 'description', 'level'];
+}
