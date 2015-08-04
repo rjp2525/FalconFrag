6d9e276cc18aba779f8e39733ff5691870cefefd
rjp2525 <me@renophilibert.com>
2015-08-04 10:30:11 -0400

    Changed from local revision module to sofa/revisionable package

---
+++ config/sofa_revisionable.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User provider (auth) implementation.
+    |--------------------------------------------------------------------------
+    |
+    | By default Laravel generic Illuminate\Auth\Guard.
+    |
+    | Supported options:
+    |  - illuminate
+    |  - sentry
+    |  - sentinel
+    */
+    'userprovider' => 'illuminate',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | User field to be saved as the author of tracked action.
+    |--------------------------------------------------------------------------
+    |
+    | By default:
+    |
+    |  - id for illuminate
+    |  - login field (email) for sentry/sentinel
+    */
+    'userfield'    => null,
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table used for the revisions.
+    |--------------------------------------------------------------------------
+    */
+    'table'        => 'revisions',
+
+];
