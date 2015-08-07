8883e2b193300a6c1967b21719fe1ece675b879b
rjp2525 <me@renophilibert.com>
2015-08-07 12:42:18 -0400

    Added a refresh database artisan command

---
+++ app/Console/Commands/RefreshDatabase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Falcon\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class RefreshDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Drop all database tables and run the migrations.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tables = [];
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        foreach (DB::select('SHOW TABLES') as $k => $v) {
+            $tables[] = array_values((array) $v)[0];
+        }
+
+        foreach ($tables as $table) {
+            Schema::drop($table);
+            $this->info('Table ' . $table . ' has been dropped.');
+        }
+
+        $this->call('migrate');
+    }
+}
